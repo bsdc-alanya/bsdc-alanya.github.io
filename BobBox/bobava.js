0497d98eae3a386cbbdc1f8b0be40ed0dce1127f
bsdc-alanya <30271090@bsdc.ac.uk>
2024-11-14 10:18:42 +0000

    bob

---
+++ BobBox/bobava.js
@@ -0,0 +1,57 @@
+console.log("this is bob");
+
+console.log(" this is bob");
+console.log("  this is bob");
+console.log("   this is bob");
+console.log("    this is bob");
+console.log("     this is bob");
+console.log("      this is bob");
+console.log("       this is bob");
+console.log("        this is bob");
+console.log("         this is bob");
+console.log("          this is bob");
+console.log("           this is bob");
+console.log("            this is bob");
+console.log("             this is bob");
+console.log("              this is bob");
+console.log("               this is bob");
+console.log("                this is bob");
+console.log("                 this is bob");
+console.log("                  this is bob");
+console.log("                   this is bob");
+console.log("                    this is bob");
+console.log("                     this is bob");
+console.log("                      this is bob");
+console.log("                       this is bob");
+console.log("                        this is bob");
+console.log("                         this is bob");
+console.log("                          this is bob");
+console.log("                           this is bob");
+console.log("                            this is bob");
+console.log("                             this is bob");
+console.log("                              this is bob");
+console.log("                               this is bob");
+console.log("                                this is bob");
+console.log("                                 this is bob");
+console.log("                                  this is bob");
+console.log("                                   this is bob");
+console.log("                                    this is bob");
+console.log("                                     this is bob");
+console.log("                                      this is bob");
+console.log("                                       this is bob");
+console.log("                                        this is bob");
+console.log("                                         this is bob");
+
+
+const bob = document.getElementById("bobinarob");
+console.log(bob.innerHTML);
+
+let switchbob = false;
+
+function BYEBOB(){
+    if (switchbob) {
+        bob.innerHTML = `<a1>yes</a1s>
+        <img scr="download-removebg-preview" alt="bob">`;
+    }
+    switchbob = !switchbob
+}
