f81a8ad028bd041e49dccc2231457fa3acd5b563
bsdc-alanya <30271090@bsdc.ac.uk>
2024-11-27 09:50:10 +0000

    pokedex

--- buttons/butava.js
+++ buttons/butava.js
@@ -1,8 +1,14 @@
-console.log(button is on)
+console.log("ITS WORKING!!! QUICK TRY IT AND PRESS THE BUITTON")
 
 const grabMyButton = document.getElementById('myButton')
 
+const myFunc = function () {
+    console.log('its working!!1');
+    console.log('its working!!2');
+    console.log('its working!!3');
+}
 
+grabMyButton.addEventListener("click", myFunc)
 
 // const myFunc = function () {
 //     console.log(`it worked`);
