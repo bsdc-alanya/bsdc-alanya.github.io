8282680dfec170dcd932ceb3584ee04ca3128048
bsdc-alanya <30271090@bsdc.ac.uk>
2024-11-27 10:29:54 +0000

    asd

--- buttons/butava.js
+++ buttons/butava.js
@@ -1,4 +1,4 @@
-console.log("ITS WORKING!!! QUICK TRY IT AND PRESS THE BUITTON")
+console.log("ITS WORKING!!! QUICK TRY IT AND PRESS THE BUTTON")
 
 const grabMyButton = document.getElementById('myButton')
 
@@ -13,3 +13,15 @@ grabMyButton.addEventListener("click", myFunc)
 // const myFunc = function () {
 //     console.log(`it worked`);
 // }
+
+const grabMyButton2 = document.getElementById("myButton2");
+const grabBody = document.querySelector("body");
+
+grabMyButton2.addEventListener("click", function () {
+    console.log(grabBody.className);
+    if (grabBody.className === "red") {
+        grabBody.className = "blue";
+    } else {
+        grabBody.className = "red"
+    }
+});
